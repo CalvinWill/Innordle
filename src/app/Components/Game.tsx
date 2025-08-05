c6eb62a3fa6aeb8644dce27f7f464518b7d4a062
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2025-08-05 17:43:00 -0400

    Halrac

--- src/app/Components/Game.tsx
+++ src/app/Components/Game.tsx
@@ -53,7 +53,7 @@ function Modal({ onClose, resetFunc, setDaily, settingsModalFunc, allCharacterDa
   const daysToCheck = 14;
   const usedIndexes = new Set<number>();
   const usedDate = new Date();
-  usedDate.setDate(usedDate.getDate())
+  usedDate.setDate(usedDate.getDate() + 1)
 
 
   // Get list of indexes from the previous 14 days
